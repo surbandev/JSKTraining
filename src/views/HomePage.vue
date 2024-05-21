9dd9385a36dcfb54a7319fef4aa819afc1405f01
Shawn Urban <surbanwebdev@gmail.com>
2024-05-21 15:41:52 -0400

    Add calender

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -22,8 +22,6 @@
         <section class="section">
         </section>
         <section class="section">
-        </section>
-        <section class="section">
           <IonButton expand="block" class="start-button" style="color: white" @click="goToDetails">Get Started!</IonButton>
         </section>
       </div>
@@ -42,6 +40,35 @@
         <section class="section">
         </section>
       </div>
+      <!-- CONTACT US SECTION -->
+      <div class="contact-section">
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <h2 class="center">Contact Us</h2>
+              <form @submit.prevent="handleSubmit">
+                <IonItem>
+                  <IonLabel position="floating" class="margin-vertical-10">Name</IonLabel>
+                  <IonInput v-model="name" type="text" required></IonInput>
+                </IonItem>
+                <IonItem>
+                  <IonLabel position="floating" class="margin-vertical-10">Email</IonLabel>
+                  <IonInput v-model="email" type="email" required></IonInput>
+                </IonItem>
+                <IonItem>
+                  <IonLabel position="floating" class="margin-vertical-10">Subject</IonLabel>
+                  <IonInput v-model="subject" type="text" required></IonInput>
+                </IonItem>
+                <IonItem>
+                  <IonLabel position="floating" class="margin-vertical-10">Message</IonLabel>
+                  <IonTextarea v-model="message" required></IonTextarea>
+                </IonItem>
+                <IonButton expand="block" type="submit">Submit</IonButton>
+              </form>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </div>
     </IonContent>
   </IonPage>
 </template>
@@ -54,20 +81,63 @@ import {
   IonHeader,
   IonPage,
   IonToolbar,
-  IonFabButton,
-  IonIcon,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonTextarea,
+  IonGrid,
+  IonRow,
+  IonCol
 } from '@ionic/vue';
-import { add } from 'ionicons/icons';
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      name: '',
+      email: '',
+      subject: '',
+      message: ''
+    };
+  },
   methods: {
     goToDetails() {
       this.$router.push({ name: 'SignUpPage' });
+    },
+    handleSubmit() {
+      if (this.name && this.email && this.subject && this.message) {
+        // Handle form submission
+        console.log('Form submitted:', {
+          name: this.name,
+          email: this.email,
+          subject: this.subject,
+          message: this.message
+        });
+        // Clear the form fields
+        this.name = '';
+        this.email = '';
+        this.subject = '';
+        this.message = '';
+        // Display a success message or redirect as needed
+        alert('Thank you for contacting us!');
+      } else {
+        alert('Please fill in all fields.');
+      }
     }
   },
-  components: { IonCardTitle, IonToolbar, IonHeader, IonButton, IonPage, IonContent, IonFabButton, IonIcon }
+  components: { IonCardTitle, IonToolbar, IonHeader, IonButton, IonPage, IonContent, IonItem, IonLabel, IonInput, IonTextarea, IonGrid, IonRow, IonCol }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.center {
+  text-align: center;
+}
+.margin-vertical-10 {
+  margin: 10px 0;
+}
+.contact-section {
+  padding: 20px;
+  background-color: #121212;
+}
+</style>
--- src/views/SignUpPage.vue
+++ src/views/SignUpPage.vue
@@ -2,104 +2,62 @@
   <IonPage>
     <IonHeader>
       <IonToolbar>
-        <ion-buttons slot="start">
-            <ion-back-button default-href="/home"></ion-back-button>
-          </ion-buttons>
-        <IonTitle>Contact Us</IonTitle>
+        <IonButton slot="start">
+          <IonBackButton default-href="/home"></IonBackButton>
+        </IonButton>
+        <IonTitle>Select Time Periods</IonTitle>
       </IonToolbar>
     </IonHeader>
-    <IonContent>
-      <IonGrid>
-        <IonRow>
-          <IonCol>
-            <form @submit.prevent="handleSubmit">
-              <IonItem>
-                <IonLabel position="floating">Name</IonLabel>
-                <IonInput v-model="name" type="text" required></IonInput>
-              </IonItem>
-              <IonItem>
-                <IonLabel position="floating">Email</IonLabel>
-                <IonInput v-model="email" type="email" required></IonInput>
-              </IonItem>
-              <IonItem>
-                <IonLabel position="floating">Subject</IonLabel>
-                <IonInput v-model="subject" type="text" required></IonInput>
-              </IonItem>
-              <IonItem>
-                <IonLabel position="floating">Message</IonLabel>
-                <IonTextarea v-model="message" required></IonTextarea>
-              </IonItem>
-              <IonButton expand="block" type="submit">Submit</IonButton>
-            </form>
-          </IonCol>
-        </IonRow>
-      </IonGrid>
+    <IonContent class="ion-padding">
+      <vue-cal style="height: 600px" :selected="selectedPeriods" @time-selected="onTimeSelected" time="12:00"
+        hide-view-selector default-view="week" draggable events-on-month-view :events="events" :time="{
+          start: '07:00',
+          end: '19:00',
+          step: '30'
+        }" />
+      <IonButton expand="block" @click="confirmSelection">Confirm Selection</IonButton>
     </IonContent>
   </IonPage>
 </template>
 
 <script>
 import {
-  IonPage,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
+IonBackButton,
+  IonButton,
   IonContent,
-  IonGrid,
-  IonRow,
-  IonCol,
-  IonItem,
-  IonLabel,
-  IonInput,
-  IonTextarea,
-  IonButton
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar
 } from '@ionic/vue';
+import VueCal from 'vue-cal';
+import 'vue-cal/dist/vuecal.css';
 
 export default {
-  name: 'ContactUs',
+  name: 'TimePeriodSelector',
   components: {
-    IonPage,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
+    IonButton,
     IonContent,
-    IonGrid,
-    IonRow,
-    IonCol,
-    IonItem,
-    IonLabel,
-    IonInput,
-    IonTextarea,
-    IonButton
-  },
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    VueCal,
+    IonBackButton
+},
   data() {
     return {
-      name: '',
-      email: '',
-      subject: '',
-      message: ''
+      selectedPeriods: [],
+      events: []
     };
   },
   methods: {
-    handleSubmit() {
-      if (this.name && this.email && this.subject && this.message) {
-        // Handle form submission
-        console.log('Form submitted:', {
-          name: this.name,
-          email: this.email,
-          subject: this.subject,
-          message: this.message
-        });
-        // Clear the form fields
-        this.name = '';
-        this.email = '';
-        this.subject = '';
-        this.message = '';
-        // Display a success message or redirect as needed
-        alert('Thank you for contacting us!');
-      } else {
-        alert('Please fill in all fields.');
-      }
+    onTimeSelected(timePeriod) {
+      this.selectedPeriods.push(timePeriod);
+    },
+    confirmSelection() {
+      console.log('Selected periods:', this.selectedPeriods);
+      alert('Time periods selected successfully!');
     }
   }
 };
